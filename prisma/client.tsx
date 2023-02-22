173b18b8f5774d122f90dffbd99ae2d11cd408b0
Gabriel Sabella <gabe.sabella.dev@gmail.com>
2023-02-22 10:55:25 -0500

    changed client variable

--- prisma/client.tsx
+++ prisma/client.tsx
@@ -1,20 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  namespace NodeJS {
-    interface Global {}
-  }
-}
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
-// add prisma to the NodeJS global type
-interface CustomNodeJsGlobal extends NodeJS.Global {
-  prisma: PrismaClient;
-}
-
-// prevent multiple instances of Prisma Client in development
-declare const global: CustomNodeJsGlobal;
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === "development") global.prisma = prisma;
-
-export default prisma;
+export default client;
